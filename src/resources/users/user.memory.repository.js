5b00b5795a79ac0497c4769a37be8006dfb74686
Kkasya <kasya--k@yandex.by>
2021-05-23 12:38:05 +0300

    feat: jsdoc for user.memory

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,7 +1,19 @@
 const DB = require('../../common/Db');
 const User = require('./user.model');
 
+
+/**
+ * Get all entries of Users
+ * @return {Array<User>}
+ */
+
 const getAll = async () => DB.getAll('Users');
+
+/**
+ * Get the entry of Users by the id
+ * @param {String} id - the id of the entry
+ * @return {User}
+ */
 
 const getById = async (id) => {
   const user = await DB.getById('Users', id);
@@ -11,13 +23,31 @@ const getById = async (id) => {
   return user;
 };
 
+/**
+ * Create new User
+ * @param {User} User - the new User
+ * @return {User}
+ */
+
 const setNew = async ({name, login, password}) => {
   const user = new User({name, login, password});
   return DB.create('Users', user);
 };
 
+/**
+ * Update the user
+ * @param {String} id - user id
+ * @param {User} updatedUser - the updated user
+ * @return {User}
+ */
 
 const update = async (id, updatedUser) => DB.update('Users', id, updatedUser);
+
+/**
+ * Remove the user by the id
+ * @param {String} id - the user id
+ * @return {void}
+ */
 
 const remove = async (id) => {
   await DB.updateUserIdOfTask(id);
