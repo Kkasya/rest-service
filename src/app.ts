136bb61b891fa929fa202c18404eaf1ea29fcc99
Kkasya <kasya--k@yandex.by>
2021-06-06 21:14:03 +0300

    fix: 1 func to write error

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
 import expressWinston from "express-winston";
-import fs from 'fs';
 import {INTERNAL_SERVER_ERROR, getStatusText} from 'http-status-codes';
 import {router  as userRouter } from './resources/users/user.router';
 import {router  as boardRouter } from './resources/boards/board.router';
 import {router  as taskRouter } from './resources/tasks/task.router';
-import {logger,ValidationError} from "./logger";
+import {logger,ValidationError, ErrorHandling} from "./logger";
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -35,39 +34,23 @@ app.use((req:Request) => {
 app.use((err:Error, _req:Request, res:Response, next:NextFunction) => {
   if (err instanceof ValidationError) {
     res.status(err.status).send(err.text);
-    fs.appendFileSync('./log/error.log', `\nVadidation error: ${err.status} ${err.text}`);
-    console.log(`\nVadidation error: ${err.status} ${err.text}` );
+    ErrorHandling( `\nVadidation error: ${err.status} ${err.text}`);
     return;
   }
   next(err);
 });
 
 app.use((_err:Error, _req:Request, res:Response) => {
-  fs.appendFileSync('./log/error.log', `\n ${getStatusText(INTERNAL_SERVER_ERROR)} ${INTERNAL_SERVER_ERROR}` );
-  console.log(`\nError: ${INTERNAL_SERVER_ERROR} ${getStatusText(INTERNAL_SERVER_ERROR)}` );
   res.status(INTERNAL_SERVER_ERROR).send(getStatusText(INTERNAL_SERVER_ERROR));
-  process.exit(1);
+  ErrorHandling(`${getStatusText(INTERNAL_SERVER_ERROR)} ${INTERNAL_SERVER_ERROR}`);
 });
-
-// setTimeout(() => {
-//   throw new Error('uncaughtException');
-// }, 1500);
 
 process.on('uncaughtException', (err:Error) => {
-  console.error(`Exception error: ${err.message}`);
-  fs.appendFileSync('./log/error.log', `\nException error: ${err.message}`);
-  process.exit(1);
+  ErrorHandling(`Exception error: ${err.message}`);
 });
-
-// setTimeout(() => {
-//   Promise.reject(new Error('unhandledRejection'))
-// }, 1500);
 
 process.on('unhandledRejection', (reason: Error) => {
-  console.error(`Unhandled rejection detected: ${reason.message}`);
-  fs.appendFileSync('./log/error.log', `\nUnhandled rejection detected: ${reason.message}`);
-  process.exit(1);
+  ErrorHandling(`Unhandled rejection detected: ${reason.message}`);
 });
-
 
 export {app};
--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,7 @@
 import expressWinston from 'express-winston';
-
 import winston from 'winston';
 import {BAD_REQUEST, getStatusText} from 'http-status-codes';
+import fs from "fs";
 
 const logger = expressWinston.logger({
   transports: [new winston.transports.File({ filename: './log/info.log', level: 'info' })],
@@ -11,11 +11,15 @@ const logger = expressWinston.logger({
   ignoreRoute  () { return false; }
 });
 
+const ErrorHandling = (message: string): void => {
+  console.error(message);
+  fs.appendFileSync('./log/error.log', `\n${message}`);
+  process.exit(1);
+};
 
 class ValidationError extends Error {
   status = BAD_REQUEST;
-
   text = getStatusText(this.status);
 }
 
-export {logger, ValidationError};
+export {logger, ValidationError, ErrorHandling};
