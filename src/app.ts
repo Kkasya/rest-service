2003324a12c173b70d094cc047782b7c2f5d350e
Kkasya <kasya--k@yandex.by>
2021-06-06 20:26:13 +0300

    feat: add types

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,14 @@
 import * as path from 'path';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
+import expressWinston from "express-winston";
+import fs from 'fs';
+import {INTERNAL_SERVER_ERROR, getStatusText} from 'http-status-codes';
 import {router  as userRouter } from './resources/users/user.router';
 import {router  as boardRouter } from './resources/boards/board.router';
 import {router  as taskRouter } from './resources/tasks/task.router';
-import expressWinston from "express-winston";
-import {logger} from "./logger";
-import fs from 'fs';
-import {ValidationError} from "./logger";
-
-const {INTERNAL_SERVER_ERROR, getStatusText} = require('http-status-codes');
-
-
+import {logger,ValidationError} from "./logger";
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -25,30 +21,18 @@ app.use(logger);
 expressWinston.requestWhitelist.push('body');
 expressWinston.responseWhitelist.push('body');
 
-// app.use('/', (req, res, next) => {
-//   if (req.originalUrl === '/') {
-//     res.send('Service is running!');
-//     return;
-//   }
-//   next();
-// });
-
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards/:boardId/tasks', taskRouter);
 
-
-
-app.use((req:any, _res:any, _next:any) => {
-  if (!req.query.param){
+app.use((req:Request) => {
+  if (!req.query["param"] ){
     throw new ValidationError();
   }
   throw new Error();
 });
 
-app.use((err:any, _req:any, res:any, next:any) => {
-  //if ()
-  console.log('---------')
+app.use((err:Error, _req:Request, res:Response, next:NextFunction) => {
   if (err instanceof ValidationError) {
     res.status(err.status).send(err.text);
     fs.appendFileSync('./log/error.log', `\nVadidation error: ${err.status} ${err.text}`);
@@ -58,26 +42,28 @@ app.use((err:any, _req:any, res:any, next:any) => {
   next(err);
 });
 
-app.use((_err:any, _req:any, res:any, _next:any) => {
+app.use((_err:Error, _req:Request, res:Response) => {
   fs.appendFileSync('./log/error.log', `\n ${getStatusText(INTERNAL_SERVER_ERROR)} ${INTERNAL_SERVER_ERROR}` );
   console.log(`\nError: ${INTERNAL_SERVER_ERROR} ${getStatusText(INTERNAL_SERVER_ERROR)}` );
   res.status(INTERNAL_SERVER_ERROR).send(getStatusText(INTERNAL_SERVER_ERROR));
+  process.exit(1);
 });
+
 // setTimeout(() => {
-//   throw new Error('Oops!');
+//   throw new Error('uncaughtException');
 // }, 1500);
 
-process.on('uncaughtException', (err:any, _origin:any) => {
+process.on('uncaughtException', (err:Error) => {
   console.error(`Exception error: ${err.message}`);
   fs.appendFileSync('./log/error.log', `\nException error: ${err.message}`);
   process.exit(1);
 });
 
 // setTimeout(() => {
-//   Promise.reject(new Error('Oops!'))
+//   Promise.reject(new Error('unhandledRejection'))
 // }, 1500);
 
-process.on('unhandledRejection', (reason:any, _promise) => {
+process.on('unhandledRejection', (reason: Error) => {
   console.error(`Unhandled rejection detected: ${reason.message}`);
   fs.appendFileSync('./log/error.log', `\nUnhandled rejection detected: ${reason.message}`);
   process.exit(1);
--- src/logger.ts
+++ src/logger.ts
@@ -1,18 +1,20 @@
-const winston = require('winston');
 import expressWinston from 'express-winston';
-const {BAD_REQUEST, getStatusText} = require('http-status-codes');
+
+import winston from 'winston';
+import {BAD_REQUEST, getStatusText} from 'http-status-codes';
 
 const logger = expressWinston.logger({
   transports: [new winston.transports.File({ filename: './log/info.log', level: 'info' })],
   msg: "",
   expressFormat: true,
   colorize: true,
-  ignoreRoute: function (_req:any, _res:any) { return false; }
+  ignoreRoute  () { return false; }
 });
 
 
 class ValidationError extends Error {
   status = BAD_REQUEST;
+
   text = getStatusText(this.status);
 }
 
